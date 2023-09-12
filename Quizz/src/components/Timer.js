fc8e02a9e142267364734d02c5073030b32cae73
Antonio Morales <126864866+Antoniomorales17@users.noreply.github.com>
2023-09-12 10:58:30 +0000

    sounds

--- Quizz/src/components/Timer.js
+++ Quizz/src/components/Timer.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+
+import React, { useEffect, useState} from "react";
 
 export default function Timer({ setTimeOut, questionNumber }) {
   const [timer, setTimer] = useState(30);
